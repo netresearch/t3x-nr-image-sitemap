22016e0742db02d3e4b09da0c2cea9679a0dd7c1
Philipp Altmann <philipp.altmann@netresearch.de>
2024-06-28 10:24:06 +0000

    NEXT-44: change path to phpstan.neon

--- Build/rector.php
+++ Build/rector.php
@@ -36,7 +36,7 @@ return static function (RectorConfig $rectorConfig): void {
         'ext_*.sql',
     ]);
 
-    $rectorConfig->phpstanConfig('Build/phpstan.neon');
+    $rectorConfig->phpstanConfig('phpstan.neon');
     $rectorConfig->importNames();
     $rectorConfig->removeUnusedImports();
     $rectorConfig->disableParallel();
