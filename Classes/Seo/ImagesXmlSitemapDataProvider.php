3557f357072600f24e36c9329cf1b438a4cb1a85
Sebastian Altenburg <sebastian.altenburg@iot-423.local>
2025-02-03 08:09:39 +0100

    NEXT-77: Use standard timestamp format in ImagesXmlSitemapDataProvider
    - Replaced underscore-separated timestamp (1_652_249_698) with standard format (1652249698) in

--- Classes/Seo/ImagesXmlSitemapDataProvider.php
+++ Classes/Seo/ImagesXmlSitemapDataProvider.php
@@ -80,7 +80,7 @@ class ImagesXmlSitemapDataProvider extends AbstractXmlSitemapDataProvider
         if ($tables === []) {
             throw new MissingConfigurationException(
                 'No configuration found for sitemap ' . $this->getKey(),
-                1_652_249_698
+                1652249698
             );
         }
 
