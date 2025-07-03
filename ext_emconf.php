39bcec0c9b5c3855eeb6780b23057ece9a1af3a4
Gitsko <s.koschel@hotmail.de>
2025-07-03 14:50:45 +0200

    [TASK] Update ext_emconf.php to load in TER via tailor

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,18 +7,6 @@
  * LICENSE file that was distributed with this source code.
  */
 
-declare(strict_types=1);
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "nr_image_sitemap".
- *
- * Auto generated 14-12-2020 09:00
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title'       => 'Netresearch - Sitemap Extension',
     'description' => 'Provides a data provider to use with the typo3/cms-seo extension, to create an image sitemap',
